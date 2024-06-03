f1aca41d0b9d454bcc11d34366a4340962f05235
Nguyen Tran <tranduccaonguyen@gmail.com>
2024-06-03 22:24:35 +0700

    JDBC

---
+++ hotel_managment_app/src/main/java/io/catnikq/hotel_app/Database.java
@@ -0,0 +1,15 @@
+package io.catnikq.hotel_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String URL = "jdbc:postgresql://localhost:5432/java_hotel_db"; // Update with your database URL
+    private static final String USER = "postgres"; // Update with your database username
+    private static final String PASSWORD = "123"; // Update with your database password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
