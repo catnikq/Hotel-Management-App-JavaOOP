0e7eeed1a4e327128ab0c6bde5b723221a612a19
Nguyen Tran <tranduccaonguyen@gmail.com>
2024-06-03 23:32:59 +0700

    Update Database.java

--- hotel_managment_app/src/main/java/io/catnikq/hotel_app/Database.java
+++ hotel_managment_app/src/main/java/io/catnikq/hotel_app/Database.java
@@ -8,8 +8,14 @@ public class Database {
     private static final String URL = "jdbc:postgresql://localhost:5432/java_hotel_db"; // Update with your database URL
     private static final String USER = "postgres"; // Update with your database username
     private static final String PASSWORD = "123"; // Update with your database password
-
+    
+    private Database(){
+        
+    };
+    
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
     }
 }
