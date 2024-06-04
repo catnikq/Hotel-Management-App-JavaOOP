9461be11bc342d2e9d7efbd8edf9f18d4af4eca4
Nguyen Tran <tranduccaonguyen@gmail.com>
2024-06-04 16:22:29 +0700

    Update Insert

--- hotel_managment_app/src/main/java/io/catnikq/hotel_app/DAO/PersonDAOImpl.java
+++ hotel_managment_app/src/main/java/io/catnikq/hotel_app/DAO/PersonDAOImpl.java
@@ -16,34 +16,56 @@ public class PersonDAOImpl implements PersonDAO {
     public Person get(int id) throws SQLException {
         Connection con = Database.getConnection();
         Person person = null;
-        
+
         String sql = "SELECT id, name, address, phone_number, age, gender FROM person WHERE id = ?";
         PreparedStatement ps = con.prepareStatement(sql);
-        
+
         ps.setInt(1, id);
-        
+
         ResultSet rs = ps.executeQuery();
-        
+
         if (rs.next()) {
-			int oid = rs.getInt("id");
-			String name = rs.getString("name");
-			String address = rs.getString("address");
-			String phone_number = rs.getString("phone_number");
-                        int age = rs.getInt("age");
-                        String gender =rs.getString("gender");
+            int oid = rs.getInt("id");
+            String name = rs.getString("name");
+            String address = rs.getString("address");
+            String phone_number = rs.getString("phone_number");
+            int age = rs.getInt("age");
+            String gender = rs.getString("gender");
 
-			person = new Person(oid, name, address, phone_number, age, gender);
-		}
+            person = new Person(oid, name, address, phone_number, age, gender);
+        }
 
-	Database.closeResultSet(rs);
-	Database.closePreparedStatement(ps);
+        Database.closeResultSet(rs);
+        Database.closePreparedStatement(ps);
         Database.closeConnection(con);
         return person;
     }
 
     @Override
     public List<Person> getAll() throws SQLException {
-        return null;
+        Connection con = Database.getConnection();
+        String sql = "SELECT id, name, address, phone_number, age, gender FROM person";
+
+        List<Person> person = new ArrayList<>();
+
+        Statement stmt = con.createStatement();
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String address = rs.getString("address");
+            String phone_number = rs.getString("phone_number");
+            int age = rs.getInt("age");
+            String gender = rs.getString("gender");
+
+            Person newperson = new Person(id, name, address, phone_number, age, gender);
+
+            person.add(newperson);
+        }
+
+        return person;
     }
 
     @Override
@@ -53,7 +75,24 @@ public class PersonDAOImpl implements PersonDAO {
 
     @Override
     public int insert(Person person) throws SQLException {
-        return 0;
+        Connection con = Database.getConnection();
+		
+		String sql = "INSERT INTO person (name, address, phone_number, age, gender) VALUES (?, ?, ?, ?, ?)";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, person.getName());
+                ps.setString(2, person.getAddress());
+                ps.setString(3, person.getPhoneNumber());
+                ps.setInt(4, person.getAge());
+                ps.setString(5, person.getGender());
+		
+		int result = ps.executeUpdate();
+		
+		Database.closePreparedStatement(ps);
+		Database.closeConnection(con);
+		
+		return result;
     }
 
     @Override
@@ -65,5 +104,5 @@ public class PersonDAOImpl implements PersonDAO {
     public int delete(Person person) {
         return 0;
     }
-    
+
 }
